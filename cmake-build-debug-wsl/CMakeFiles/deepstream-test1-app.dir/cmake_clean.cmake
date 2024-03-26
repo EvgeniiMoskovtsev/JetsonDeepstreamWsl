2fb1eb6925feceb82e629063c51d4483773166e5
Evgenii Moskovtsev <apelvery@gmail.com>
2024-03-26 13:16:42 +0400

    release: 0.0.1

---
+++ cmake-build-debug-wsl/CMakeFiles/deepstream-test1-app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deepstream-test1-app.dir/src/deepstream_test1_app.cpp.o"
+  "deepstream-test1-app"
+  "deepstream-test1-app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deepstream-test1-app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
